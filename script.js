abad99c9cf83a9f71606aadea57e4f33fe5ee71c
liolle <laurentlionel20@hotmail.com>
2022-12-08 15:50:20 +0100

    Auto typing

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const themes = ["indigo-white",
     "green-white",
     "red-white",
@@ -7,7 +6,9 @@ const themes = ["indigo-white",
     "white-grey",
     "white-red",
     "yellow-black"]
+
 let index =0
+
 
 
 // SetUp Listener
@@ -35,16 +36,16 @@ right_bnt.addEventListener('click',()=>{
 
 function setTheme(){  
     let current_theme = document.querySelector("link[href^='css/themes/']")
-
+    
     if (current_theme == undefined){
         let ct = document.createElement("link")
-
+        
         ct.type = 'text/css';
         ct.rel = 'stylesheet';
         ct.href = `css/themes/${themes[index]}.css`
-
+        
         head.appendChild(ct)
-
+        
         return
         
     }
@@ -53,3 +54,98 @@ function setTheme(){
 }
 
 setTheme()
+
+/*
+All-Star Dev | Code Fanatic | Linux Hacker | Bleh
+*/
+
+// Typing 
+
+const title = [ 
+    "All-Star Dev", 
+    "JVM, GCC, Py, ...", 
+    "Manga enjoyer"
+];
+
+// Current sentence being processed
+var _PART = 0;
+
+// Character number of the current sentence being processed 
+var _PART_INDEX = 0;
+
+// Holds the handle returned from setInterval
+var _INTERVAL_VAL;
+
+// Element that holds the text
+var _ELEMENT = document.querySelector(".typed");
+
+// Cursor element 
+var _CURSOR = document.querySelector(".cursor");
+
+// Implements typing effect
+function Type() { 
+	// Get substring with 1 characater added
+	let text =  title[_PART].substring(0, _PART_INDEX + 1);
+	_ELEMENT.innerHTML = text;
+	_PART_INDEX++;
+
+	// If full sentence has been displayed then start to delete the sentence after some time
+	if(text === title[_PART]) {
+		// Hide the cursor
+		_CURSOR.style.display = 'none';
+
+		clearInterval(_INTERVAL_VAL);
+		setTimeout(function() {
+			_INTERVAL_VAL = setInterval(Delete, 50);
+		}, 1000);
+	}
+}
+
+// Implements typing effect
+function Type() { 
+	// Get substring with 1 characater added
+	var text =  title[_PART].substring(0, _PART_INDEX + 1);
+	_ELEMENT.innerHTML = text;
+	_PART_INDEX++;
+
+	// If full sentence has been displayed then start to delete the sentence after some time
+	if(text === title[_PART]) {
+		// Hide the cursor
+		_CURSOR.style.display = 'none';
+
+		clearInterval(_INTERVAL_VAL);
+		setTimeout(function() {
+			_INTERVAL_VAL = setInterval(Delete, 50);
+		}, 5000);
+	}
+}
+
+// Implements deleting effect
+function Delete() {
+	// Get substring with 1 characater deleted
+	var text =  title[_PART].substring(0, _PART_INDEX - 1);
+	_ELEMENT.innerHTML = text;
+	_PART_INDEX--;
+
+	// If sentence has been deleted then start to display the next sentence
+	if(text === '') {
+		clearInterval(_INTERVAL_VAL);
+
+		// If current sentence was last then display the first one, else move to the next
+		if(_PART == (title.length - 1))
+			_PART = 0;
+		else
+			_PART++;
+		
+		_PART_INDEX = 0;
+
+		// Start to display the next sentence after some time
+		setTimeout(function() {
+			_CURSOR.style.display = 'inline-block';
+			_INTERVAL_VAL = setInterval(Type, 100);
+		}, 200);
+	}
+}
+
+// Start the typing effect on load
+_INTERVAL_VAL = setInterval(Type, 100);
